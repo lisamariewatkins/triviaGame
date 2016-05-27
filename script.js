0fc3521cb90887ef30e2daa29f35696d1585139e
Lisa Watkins <Lisa Watkins>
2016-05-26 21:18:50 -0500

    style updates

--- script.js
+++ script.js
@@ -1,17 +1,20 @@
 var triviaArray = [{
 		name: "Mark Bradford",
+		description: "<strong>Mark Bradford</strong> created this piece in 2015.  He used torn pieces of paper, shellac, and oil.",
 		title: "Let's Walk to the Middle of the Ocean",
 		img: "mb.jpg",
 		choices: ["Jackson Pollack", "Georgia O'Keefe", "Pablo Picasso", "Mark Bradford"]
 	},
 	{
 		name: "Vincent Van Gogh",
+		description: "bla bla",
 		title: "Starry Night",
 		img: "vg.jpg",
 		choices: ["Jackson Pollack", "Georgia O'Keefe", "Pablo Picasso", "Vincent Van Gogh"]
 	},
 	{
 		name: "Jackson Pollack",
+		description: "bla bla",
 		title: "Number One",
 		img: "jp.jpg",
 		choices: ["Vincent Van Gogh", "Georgia O'Keefe", "Pablo Picasso", "Jackson Pollack"]
@@ -31,6 +34,8 @@ var number = 10;
 
 var counter;
 
+var unanswered = 0;
+
 function questionTimer(){
 	counter = setInterval(decrement, 1000);
 }
@@ -41,12 +46,14 @@ function stop(){
 
 function decrement(){
 	number--;
-	$('#timer').html(number);
+	$('#timer').html("<p><strong>" + number + " seconds left</strong></p>");
 	if(number === 0){
 		stop();
 		triviaArray.splice(artistIndex, 1);
-		empty();
-		$("#instructions").html("You've run out of time!");
+		unanswered++;
+		$("#instructions").html("<p>You've run out of time!</p>");
+		$("#timer").empty();
+		$("#answerChoices").html("<p class='options'>" + targetArtist.description + "</p>");
 		$("#eventButton").html("<button class='button'>Next piece</button>")
 		if(triviaArray.length === 0){
 			empty();
@@ -60,7 +67,7 @@ function populateQuestion(){
 	artistIndex = triviaArray.indexOf(targetArtist); //the splice method called later needs two numbers, so i need to store the numerical index value of targetArtist
 	//populate instructions/disply
 	$("#instructions").html("<p id='title'>" + targetArtist.title + "</p>");
-	$("#image").html("<img class=artImage src='" + targetArtist.img + "'>");
+	$("#image").html("<p><img class=artImage src='" + targetArtist.img + "'></p>");
 	//reset timer
 	number = 10;
 	//start timer
@@ -86,23 +93,27 @@ function checkAnswer(){
 		stop();
 		if (guessedName === targetArtist.name){
 			wins++;
-			$("#instructions").html("<p>Correct! On to the next art piece!</p><br>");
+			$("#instructions").html("<p>Correct!</p>");
+			$("#timer").empty();
+			$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
 			$("#eventButton").html("<button class='button'>Next piece</button>");
 			}
 		else{
 			$("#instructions").html("<p>Incorrect!</p>");
+			$("#timer").empty();
+			$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
 			$("#eventButton").html("<button class='button'>Next piece</button>");
 		}
 		if(triviaArray.length === 0){
 			empty();
-			$("#instructions").html("You answered " + wins + " out of " + originalTriviaArrayLength + " correctly!")
+			$("#instructions").html("You answered " + wins + " out of " + originalTriviaArrayLength + " correctly!<br><p>You left " + unanswered + " unanswered.")
 		}
 	});
 }
 
 $(document).ready(function(){
 	$("#mainText").html("<h1>Museum of Modern Art Trivia Game</h1>");
-	$("#instructions").html("<div id='instructions'>Test your framiliarity with 20th century modern artists.  A photo of the artwork and the name of the artwork will be provided, just match the name of the artist to his or her piece.  All pieces in this trivia game were selected from the permanent collection of the Museum of Modern Art (MoMA) in New York City.  Press the button below to get started.</div> <br><br><button class='button'>Begin</button>");
+	$("#instructions").html("<div id='instructions'>Test your familiarity with 20th century modern artists.  A photo of the artwork and the name of the artwork will be provided, just match the name of the artist to his or her piece.  All pieces in this trivia game were selected from the permanent collection of the Museum of Modern Art (MoMA) in New York City.  Press the button below to get started.</div> <br><br><button class='button'>Begin</button>");
 	$(".button").on('click', function(){
 		populateQuestion();
 		checkAnswer();
