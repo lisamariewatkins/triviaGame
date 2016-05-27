d67f0747b358718d650b5449471c595f9414c9b6
Lisa Watkins <Lisa Watkins>
2016-05-27 16:18:35 -0500

    fixed last question

--- script.js
+++ script.js
@@ -1,25 +1,24 @@
 var triviaArray = [{
 		name: "Mark Bradford",
-		description: "<strong>Mark Bradford</strong> created this piece in 2015.  He used torn pieces of paper, shellac, and oil.",
+		description: "<strong>Mark Bradford</strong> created this piece in 2015.  He used torn pieces of paper, shellac, and oil on canvas.",
 		title: "Let's Walk to the Middle of the Ocean",
 		img: "mb.jpg",
 		choices: ["Jackson Pollack", "Georgia O'Keefe", "Pablo Picasso", "Mark Bradford"]
 	},
 	{
 		name: "Vincent Van Gogh",
-		description: "bla bla",
+		description: "<strong>Vincent Van Gogh</strong> created this piece in 1889.  He used oil on canvas.",
 		title: "Starry Night",
 		img: "vg.jpg",
 		choices: ["Jackson Pollack", "Georgia O'Keefe", "Pablo Picasso", "Vincent Van Gogh"]
 	},
 	{
 		name: "Jackson Pollack",
-		description: "bla bla",
+		description: "<strong>Jackson Pollock</strong> created this piece in 1950.  He used oil, enamel, and aluminum on canvas.",
 		title: "Number One",
 		img: "jp.jpg",
 		choices: ["Vincent Van Gogh", "Georgia O'Keefe", "Pablo Picasso", "Jackson Pollack"]
 	}
-
 ]
 
 var originalTriviaArrayLength = triviaArray.length
@@ -30,7 +29,9 @@ var artistIndex; //same here
 
 var wins = 0;
 
-var number = 10;
+var paintings = 1;
+
+var number = 10; //for timer
 
 var counter;
 
@@ -66,6 +67,7 @@ function populateQuestion(){
 	targetArtist = triviaArray[Math.floor(Math.random()*triviaArray.length)];
 	artistIndex = triviaArray.indexOf(targetArtist); //the splice method called later needs two numbers, so i need to store the numerical index value of targetArtist
 	//populate instructions/disply
+	$("#paintingsLeft").html(paintings + "/" + originalTriviaArrayLength + " peices.")
 	$("#instructions").html("<p id='title'>" + targetArtist.title + "</p>");
 	$("#image").html("<p><img class=artImage src='" + targetArtist.img + "'></p>");
 	//reset timer
@@ -76,6 +78,7 @@ function populateQuestion(){
 	for (var i = 0; i < targetArtist.choices.length; i++){
 		$("#answerChoices").append("<p class='choices'>" + targetArtist.choices[i] + "</p>");
 	}
+	paintings++;
 }
 
 function empty(){
@@ -84,6 +87,7 @@ function empty(){
 	$("#answerChoices").empty();
 	$("#timer").empty();
 	$("#eventButton").empty();
+	$("#scoreButton").empty();
 }
 
 function checkAnswer(){
@@ -91,37 +95,56 @@ function checkAnswer(){
 		var guessedName = $(this).text();
 		triviaArray.splice(artistIndex, 1); //removes artist so no duplicates
 		stop();
-		if (guessedName === targetArtist.name){
-			wins++;
-			$("#instructions").html("<p>Correct!</p>");
-			$("#timer").empty();
-			$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
-			$("#eventButton").html("<button class='button'>Next piece</button>");
+		if(triviaArray.length > 0){
+			if (guessedName === targetArtist.name){
+				wins++;
+				$("#instructions").html("<p>Correct!</p>");
+				$("#timer").empty();
+				$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
+				$("#eventButton").html("<button class='button'>Next piece</button>");
 			}
-		else{
-			$("#instructions").html("<p>Incorrect!</p>");
-			$("#timer").empty();
-			$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
-			$("#eventButton").html("<button class='button'>Next piece</button>");
+			else{
+				$("#instructions").html("<p>Incorrect!</p>");
+				$("#timer").empty();
+				$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
+				$("#eventButton").html("<button id='nextButton' class='button'>Next piece</button>");
+			}
 		}
+		
 		if(triviaArray.length === 0){
-			empty();
-			$("#instructions").html("You answered " + wins + " out of " + originalTriviaArrayLength + " correctly!<br><p>You left " + unanswered + " unanswered.")
+			if (guessedName === targetArtist.name){
+				wins++;
+				$("#instructions").html("<p>Correct!</p>");
+				$("#timer").empty();
+				$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
+				$("#eventButton").html("<button class='button'>See your score</button>");
+			}
+			else{
+				$("#instructions").html("<p>Incorrect!</p>");
+				$("#timer").empty();
+				$("#answerChoices").html("<p>" + targetArtist.description + "</p>");
+				$("#scoreButton").html("<button id='nextButton' class='button'>See your score</button>");
+			}
 		}
 	});
 }
 
 $(document).ready(function(){
-	$("#mainText").html("<h1>Museum of Modern Art Trivia Game</h1>");
-	$("#instructions").html("<div id='instructions'>Test your familiarity with 20th century modern artists.  A photo of the artwork and the name of the artwork will be provided, just match the name of the artist to his or her piece.  All pieces in this trivia game were selected from the permanent collection of the Museum of Modern Art (MoMA) in New York City.  Press the button below to get started.</div> <br><br><button class='button'>Begin</button>");
-	$(".button").on('click', function(){
+	$("#startButton").on('click', function(){
 		populateQuestion();
 		checkAnswer();
 	});
+
 	$("#eventButton").on('click', function(){
 		empty();
 		populateQuestion();
 		checkAnswer();
+	});
+	
+	$("#scoreButton").on('click', function(){
+		empty();
+		$("#paintingsLeft").empty();
+		$("#instructions").html("You answered " + wins + " out of " + originalTriviaArrayLength + " correctly!<br><p>You left " + unanswered + " unanswered.")
 	});
 
 });
